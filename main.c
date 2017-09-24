ff8531c1993af2d985aa55bc22030835c4dbc7fd
Ap4ep25 <31929500+Ap4ep25@users.noreply.github.com>
2017-09-24 22:14:49 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    return 0;
+}
+
+5 == 5
+9 == var1
+var1 == 9
+=<
+>=
+!=
+>
+<
+- истина = 0_false False_
+- ложь = 0_true True_
+Pascal, Python       (var1 == 9) and (var2 != 1)
+                                  or
+  c    (var1 == 9) && (var2 != 1)
+                   ||
+C if, switch
+Python if
+
+if (var1 == 9) && (a > 5) || (b < 7))
+{
+doSomeSeriousStuff(); // true истина
+var1 += 2;
+}
+else {
+var1 += 1;   // :))
+////
+printf("privet");
+}
+var1 = 0;
+if (1)
+    var1 += 1;
+else
+    var1 += 2;
+
+1 2 3 4 5 6
+var1 = 3;
+switch (var1)
+{
+case 0:
+    ///
+    break;
+    case 2:
+    ///
+    break;
+    case 3:
+    ///
+    break;
+    default:
+    printf("Enter 0 1 2 3");
+}
+printf("42");
+
+double result = 5.6890;
+double a = 5.6891;
+result += 0.987;
+
+    ///!!!!!
+if (result != 0.98765)
+    /// true
+else
+    /// false
+
+    ///++++
+
+if (abs(result - a) < eps)
+    ///
+else
+    //
